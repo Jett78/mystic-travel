33aa0c903df08728359db5f1acd43e0c1c3b141f
Jeet <jetdeula@gmail.com>
2025-03-03 10:42:30 +0545

    chore : add pattern below img

--- components/website/AllExpedition/AllExpHero.tsx
+++ components/website/AllExpedition/AllExpHero.tsx
@@ -14,16 +14,23 @@ export default function AllExpHero({ distance }: any) {
   const { name, route } = data;
   return (
     <>
-      <div className="w-full h-[50vh] bg-zinc-200 relative  flex justify-center items-end">
+      <div className="w-full h-[60vh] bg-zinc-200 relative  flex justify-center items-end">
         <Image
           width={1000}
           height={1000}
-          src={HeroBg}  
+          src={HeroBg}
           alt="expedition-image"
-          className="absolute top-0 left-0 w-full  bg-secondary-50  h-[50vh] object-cover object-top pt-[2rem]"
+          className=" w-full  bg-secondary-50 h-full object-cover object-top pt-[2rem]"
         />
-        <div className="mask1 absolute bottom-0 left-0 w-full h-[50%] bg-gradient-to-t from-secondary-50  to-transparent z-10"></div>
-        <h1 className="text-3xl md:text-6xl relative tracking-wide mt-10 title font-bold text-secondary-400 z-10">
+        <Image
+          width={1000}
+          height={1000}
+          src="/pattern.png"
+          alt="expedition-image"
+          className=" absolute bottom-0 w-full h-20"
+        />
+        <div className="absolute inset-0 w-full h-80 bg-gradient-to-b from-black via-black/50 to-transparent" />
+        <h1 className="text-3xl md:text-6xl absolute left-1/2 -translate-x-1/2 top-1/2 -translae-y-1/2  tracking-wide  title font-bold text-white z-10">
           Expedition - {route}
         </h1>
       </div>
--- components/website/AllTrek/AllTrekHero.tsx
+++ components/website/AllTrek/AllTrekHero.tsx
@@ -14,17 +14,25 @@ export default function AllTrekHero({ region }: any) {
   const { name } = data;
   return (
     <>
-      <div className="w-full h-[50vh] bg-zinc-200  overflow-hidden relative  flex justify-center items-center">
+      <div className="w-full h-[60vh] bg-zinc-200  overflow-hidden relative  flex justify-center items-center">
         <Leaf />
         <Image
           width={1000}
           height={1000}
           src="https://images.unsplash.com/photo-1611154046036-cd91e50978be?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="expedition-image"
-          className="absolute top-0 left-0 w-full h-[50vh] object-cover object-center"
+          className="h-full w-full object-cover object-center"
         />
-        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-[0.3]"></div>
-        <h1 className="text-3xl md:text-6xl relative tracking-wide mt-10 title font-bold text-secondary-50">
+        <Image
+          width={1000}
+          height={1000}
+          src="/pattern.png"
+          alt="expedition-image"
+          className=" absolute bottom-0 w-full h-20"
+        />
+        <div className="absolute inset-0 w-full h-80 bg-gradient-to-b from-black via-black/50 to-transparent" />
+
+        <h1 className="text-3xl md:text-6xl absolute left-1/2 -translate-x-1/2 top-1/2 -translae-y-1/2 tracking-wide title font-bold text-secondary-50">
           {name}
         </h1>
       </div>
