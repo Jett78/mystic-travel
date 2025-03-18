2b91674b34bfb7af1dce5f96f7ad73b284ea04aa
Jeet <jetdeula@gmail.com>
2025-03-18 15:46:57 +0545

    changed : design of not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="h-screen fixed inset-0 z-[999]">
+    <div className="h-screen fixed inset-0 z-[50]">
       <Image
         src="https://images.unsplash.com/photo-1454496522488-7a8e488e8606?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="404"
