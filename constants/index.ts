c22a1fff8a71a451685ac117ba7189524c498cae
Jeet <jetdeula@gmail.com>
2025-03-18 15:47:07 +0545

    seaparated nav links

---
+++ constants/index.ts
@@ -0,0 +1,41 @@
+export const aboutLinks = [
+  { name: "Company", href: "/about_us" },
+  { name: "Our team", href: "/our_team" },
+  { name: "Certificates", href: "/certificates" },
+];
+
+export const usefulInfo = [
+  { name: "Visa Info", href: "/visa_info" },
+  { name: "Travel Info", href: "/travel_info" },
+  { name: "General Info", href: "/general_info" },
+  { name: "Trekking Info", href: "/trekking_info" },
+];
+export const destinationsLinks = [
+  { name: "Nepal", href: "/nepal/everest_region" },
+  { name: "India", href: "/india/city_tours" },
+  { name: "Bhutan", href: "/bhutan/city_tours" },
+  { name: "Tibet", href: "/tibet/city_tours" },
+];
+
+export const activitiesLink = [
+  { name: "Rafting", href: "/other_activities/rafting" },
+  { name: "Heli Tour", href: "/other_activities/heli_tour" },
+  { name: "Paragliding", href: "/other_activities/paragliding" },
+  { name: "Mountain Biking", href: "/other_activities/mountain_biking" },
+  { name: "Rock Climbing", href: "/other_activities/rock_climbing" },
+];
+
+export const expeditionsLink = [
+  { name: "8000 meters", href: "/expedition/8000m" },
+  { name: "7000 meters", href: "/expedition/7000m" },
+  { name: "6000 meters", href: "/expedition/6000m" },
+  { name: "5000 meters", href: "/expedition/5000m" },
+];
+
+export const treksLink = [
+  { name: "Everest Region Trekking", href: "/trek/everest_region_trekking" },
+  {
+    name: "Annapurna Region Trekking",
+    href: "/trek/annapurna_region_trekking",
+  },
+];
