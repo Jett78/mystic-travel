4fb582399668fd1efe752ec5e4cf9823217e87a8
Jeet <jetdeula@gmail.com>
2025-03-21 16:37:18 +0545

    add : banner cmponenet for activity hero

--- components/website/OtherActivities/OtherActivitiesHero.tsx
+++ components/website/OtherActivities/OtherActivitiesHero.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import OtherActivitiesData from "@/data/OtherActivitiesData";
+import Banner from "@/components/ui/banner";
 
 type Props = {};
 
@@ -16,19 +17,11 @@ export default function OtherActivitiesHero({ activity }: any) {
 
   return (
     <>
-      <div className="w-full h-[50vh] bg-zinc-200 relative  flex justify-center items-center">
-        <Image
-          width={HeroImg.width}
-          height={HeroImg.height}
-          src={HeroImg.src}
-          alt={`${name} image`}
-          className="absolute top-0 left-0 w-full h-[50vh] object-cover object-center"
-        />
-        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-[0.5]"></div>
-        <h1 className="text-5xl md:text-6xl relative tracking-wide mt-10 title font-bold text-secondary-50">
-          {name}
-        </h1>
-      </div>
+      <Banner
+        title={name}
+        className="md:text-[4vw] text-4xl"
+        img={HeroImg.src}
+      />
     </>
   );
 }
