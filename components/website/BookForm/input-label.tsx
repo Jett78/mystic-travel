b1547e49ec188c51751713ea29bf216d254cf90d
Jeet <jetdeula@gmail.com>
2025-03-21 16:33:57 +0545

    add: css for label

--- components/website/BookForm/input-label.tsx
+++ components/website/BookForm/input-label.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const InputLabel = ({ label }: { label: string }) => {
-  return <label htmlFor={label}>{label}</label>;
+  return (
+    <label htmlFor={label} className="text-sm font-medium">
+      {label} <span className=" text-red-500">*</span>
+    </label>
+  );
 };
 
 export default InputLabel;
