b01135dae4767f219f107763662ea1aec9a73f95
Jeet <jetdeula@gmail.com>
2025-03-21 15:12:16 +0545

    updated : z index

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,19 +1,11 @@
 import React from "react";
-import LogoIcon from "@/public/logo/logo-white.svg";
-import Image from "next/image";
 
-type Props = {};
-
-export default function Loading({}: Props) {
+const Loading = () => {
   return (
-    <div className="w-full h-screen flex  justify-center items-center">
-      <div className="">
-        <Image
-          src={LogoIcon}
-          alt="trek-nepal-logo-icon"
-          className="logo-icon w-[3rem] h-[3rem] object-cover object-center"
-        ></Image>
-      </div>
+    <div className="h-screen z-[999] fixed w-full bg-white flex justify-center items-center">
+      <div className="loader"></div>
     </div>
   );
-}
+};
+
+export default Loading;
