64db02ae533883d1914c189fb92a02de24ae5ba7
Jeet <jetdeula@gmail.com>
2025-04-01 12:21:52 +0545

    changed parallax img

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,7 +66,7 @@ const config: Config = {
 
       backgroundImage: {
         parallex1:
-          'url("https://images.unsplash.com/photo-1517934274943-d1749ff2d7a8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+          'url("https://images.unsplash.com/photo-1454496522488-7a8e488e8606?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
         parallex2:
           'url("https://images.unsplash.com/reserve/91JuTaUSKaMh2yjB1C4A_IMG_9284.jpg?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
